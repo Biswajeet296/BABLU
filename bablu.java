1acc42c93ddb0b265b2565920c6f7045eb37407c
Biswajeet296 <240301120378@centurionuniv.edu.in>
2025-07-07 22:13:32 +0530

    Add files via upload

---
+++ bablu.java
@@ -0,0 +1,13 @@
+public class bablu {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the two numbers
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
