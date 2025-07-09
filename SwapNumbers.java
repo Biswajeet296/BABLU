a4d020ff2b944bf824dc92406a7bf95f93309585
Biswajeet296 <240301120378@centurionuniv.edu.in>
2025-07-09 14:49:08 +0530

    Update SwapNumbers.java

--- SwapNumbers.java
+++ SwapNumbers.java
@@ -1,4 +1,4 @@
-// Swapping two numbers using a temporary variable
+//  two numbers swapped using a temporary variable
 public class SwapNumbers {
     public static void main(String[] args) {
         int num1 = 10, num2 = 20;
