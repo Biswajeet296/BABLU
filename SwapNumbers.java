5b322b7bc217a120085a1d97f5b16566bb4790bc
Biswajeet296 <240301120378@centurionuniv.edu.in>
2025-07-09 14:45:37 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,19 @@
+// Swapping two numbers using a temporary variable
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Swapping logic
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
